af84a049f7d5cb717125aab31ea5d1bb4b785aec
Gad-Ongoro <gadongoro1@gmail.com>
2023-11-29 16:01:19 +0300

    Completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self, brand, size:int):
-        if not isinstance(size, int):
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+
+    def brand_getter(self):
+        return self._brand
+    
+    def brand_setter(self, new_brand):
+        self._brand = new_brand
+
+    def size_getter(self):
+        return self._size
+
+    def size_setter(self, new_size):
+        if type(new_size) not in (int,):
             print("size must be an integer")
-        self.brand = brand
-        self.size = size
+        self._size = new_size
+
+    brand = property(brand_getter, brand_setter)
+    size = property(size_getter, size_setter)
 
     def cobble(self):
         print("Your shoe is as good as new!")
